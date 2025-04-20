2f39655764d9916aa60ff62228fc1ff5bc98ad9c
samyak <samyaksk10@gmail.com>
2025-04-20 15:52:12 +0530

    attempting to change routes

--- client/app.js
+++ client/app.js
@@ -26,7 +26,7 @@ function getBathValue() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
   
-    var url = "http://127.0.0.1:5000/predict_home_price";
+    var url = "/predict_home_price";
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
         bhk: bhk,
@@ -41,7 +41,7 @@ function getBathValue() {
   
   function onPageLoad() {
     console.log( "document loaded" );
-    var url = "http://127.0.0.1:5000/get_location_names"; 
+    var url = "/get_location_names"; 
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
         if(data) {
